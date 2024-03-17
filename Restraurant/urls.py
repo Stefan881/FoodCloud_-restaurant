342eb7495e1a6af914438ab4d9809250b3e1862c
Ondeyo Stephen <ondeyostephen0@gmail.com>
2024-03-17 12:02:26 +0300

    Urls

--- Restraurant/urls.py
+++ Restraurant/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home-url'),
+    path('index', views.home, name='index-url'),
     path('book_a_table', views.book_a_table, name='Book-a-table-url'),
     path('contact_us', views.contact, name='contact_us-url')
 ]
